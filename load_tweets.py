42908745434fc28c5bff41747de22c18b0771fad
kjiang25 <kjiang25@cmc.edu>
2025-04-11 10:53:16 -0700

    updated sql_comit error

--- load_tweets.py
+++ load_tweets.py
@@ -109,7 +109,6 @@ def insert_tweet(connection,tweet):
         })
     if res.first() is not None:
         return
-    connection.commit()
 
     # insert tweet within a transaction;
     # this ensures that a tweet does not get "partially" loaded
