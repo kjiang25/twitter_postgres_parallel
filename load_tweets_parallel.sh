b97c9702493b5abb60c0f753225f2d629ce09d3b
kjiang25 <kjiang25@cmc.edu>
2025-04-21 13:16:58 -0700

    attempting normalized parallel

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -13,6 +13,7 @@ echo '==========================================================================
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time parallel 'python3 load_tweets.py --db postgresql://postgres:pass@localhost:10652/postgres --inputs {} --print_every 10000' ::: data/*
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
